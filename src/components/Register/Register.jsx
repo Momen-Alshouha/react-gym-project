8d311dea5b6ce701950a950a27ed8e4890aa85c3
Bahaazyoud <bahaazyoud2000@gmail.com>
2022-07-06 13:07:46 +0300

    upload

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,26 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
 import classes from "./Register.module.css";
 const Register = () => {
+  const [data, setFormValue] = useState({
+    name: null,
+    email: null,
+    password: null,
+  });
+  window.axios = require("axios");
+
+  const Submit = (event) => {
+    event.preventDefault();
+    const api = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+    axios.post("https://62c54cf0134fa108c24dabbc.mockapi.io/user", api);
+  };
+  const valueHandler = (event) => {
+    setFormValue({ ...data, [event.target.name]: event.target.value });
+  };
   return (
     <Fragment>
       <div className={classes.container}>
@@ -8,11 +28,28 @@ const Register = () => {
           <h1 className={classes.title}>REGISTER</h1>
         </div>
         <ul className={classes.parent}>
-          <form>
+          <form onSubmit={Submit}>
+            <li>
+              <label htmlFor="NAME">NAME</label>
+              <br />
+              <input
+                type="text"
+                id="NAME"
+                name="name"
+                placeholder="Write Email Here"
+                onChange={valueHandler}
+              />
+            </li>
             <li>
               <label htmlFor="EMail">E-MAIL</label>
               <br />
-              <input type="email" id="Email" placeholder="Write Email Here" />
+              <input
+                type="email"
+                id="Email"
+                name="email"
+                onChange={valueHandler}
+                placeholder="Write Email Here"
+              />
             </li>
             <li>
               <label htmlFor="password">PASSWORD</label>
@@ -20,6 +57,8 @@ const Register = () => {
               <input
                 type="password"
                 id="password"
+                name="password"
+                onChange={valueHandler}
                 placeholder="Write Password Here"
               />
             </li>
